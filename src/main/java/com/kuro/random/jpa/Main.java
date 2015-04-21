dc1a6157358b8e853ccfc1dfa39c494cfd6d8e9a
Kumar Rohit <incrediblevicky@gmail.com>
2015-04-21 18:23:02 +0530

    Restructure the maven structure

---
+++ src/main/java/com/kuro/random/jpa/Main.java
@@ -0,0 +1,7 @@
+package com.kuro.random.jpa;
+
+/**
+ * Created by Kumar Rohit on 4/21/15.
+ */
+public class Main {
+}
---
+++ src/test/java/com/kuro/random/jpa/MainTest.java
@@ -0,0 +1,10 @@
+package com.kuro.random.jpa;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Kumar Rohit on 4/21/15.
+ */
+public class MainTest {
+
+}
